3f99e3238b3aaa38b09a6d62b60bea3c9b3e0c38
RomanasV <romanasvenckus@gmail.com>
2023-01-13 15:32:02 +0200

    functions

---
+++ 2023-01-13-functions/script.js
@@ -0,0 +1,60 @@
+// UŽDUOTIS:
+// Į konsole išvesti apskaičiuotus keturkampio perimetrus pagal šias kraštines:
+// 1. 10 ilgis ir 10 plotis.
+// 2. 10 ilgis ir 25 plotis.
+// 3. 10 ilgis ir 30 plotis.
+// 4. 10 ilgis ir 35 plotis.
+// 5. 10 ilgis ir 40 plotis.
+// 6. 10 ilgis ir 45 plotis.
+// 7. 10 ilgis ir 50 plotis.
+// 8. 10 ilgis ir 55 plotis.
+// 9. 10 ilgis ir 60 plotis.
+// 10. 10 ilgis ir 120 plotis.
+
+console.log('Perimetras: ' + (10 + 10) * 2 + ' kv. cm.');
+console.log('Perimetras: ' + (10 + 25) * 2 + ' kv. cm.');
+console.log('Perimetras: ' + (10 + 30) * 2 + ' kv. cm.');
+console.log('Perimetras: ' + (10 + 35) * 2 + ' kv. cm.');
+console.log('Perimetras: ' + (10 + 40) * 2 + ' kv. cm.');
+console.log('Perimetras: ' + (10 + 45) * 2 + ' kv. cm.');
+console.log('Perimetras: ' + (10 + 50) * 2 + ' kv. cm.');
+console.log('Perimetras: ' + (10 + 55) * 2 + ' kv. cm.');
+console.log('Perimetras: ' + (10 + 60) * 2 + ' kv. cm.');
+console.log('Perimetras: ' + (10 + 120) * 2 + ' kv. cm.');
+
+// DRY - Don't Repeat Yourself
+
+// FUNKCIJOS
+// Funkcijos sukūrimas:
+// 1. Iniciavimo žodelio (function).
+// 2. Pavadinimo.
+// 3. Paprasti skliaustai () - funkcijos argumentams.
+// 4. Riestiniai skliaustai {} - funkcijos apibrėžimas.
+
+function hello() {
+  console.log('Hello, John');
+}
+
+// Funkcijos iškvietimas
+// 1. Funkcijos pavadinimą
+// 2. Paprasti skliaustai ()
+
+hello();
+
+// Funkcijos su argumentais (funkcija, kuri turi besikeičiančias dalis)
+function helloWithName(personName, personSurname = '') {
+  // console.log('Hello, ' + personName + ' ' + personSurname + '.');
+
+  // if (personSurname) {
+  //   console.log(`Hello, ${personName} ${personSurname}`);
+  // } else {
+  //   console.log(`Hello, ${personName}`);
+  // }
+
+  console.log(`Hello, ${personName} ${personSurname}`);
+}
+
+helloWithName('John', 'Doe');
+helloWithName('Steve');
+helloWithName('Peter');
+helloWithName('Mike');
