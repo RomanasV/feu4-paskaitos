1f304d9abc7eba17bbcd928b298d19852e8c3157
RomanasV <romanasvenckus@gmail.com>
2023-01-16 08:23:36 +0200

    function tasks added

--- 2023-01-13-functions/script.js
+++ 2023-01-13-functions/script.js
@@ -11,16 +11,16 @@
 // 9. 10 ilgis ir 60 plotis.
 // 10. 10 ilgis ir 120 plotis.
 
-console.log('Perimetras: ' + (10 + 10) * 2 + ' kv. cm.');
-console.log('Perimetras: ' + (10 + 25) * 2 + ' kv. cm.');
-console.log('Perimetras: ' + (10 + 30) * 2 + ' kv. cm.');
-console.log('Perimetras: ' + (10 + 35) * 2 + ' kv. cm.');
-console.log('Perimetras: ' + (10 + 40) * 2 + ' kv. cm.');
-console.log('Perimetras: ' + (10 + 45) * 2 + ' kv. cm.');
-console.log('Perimetras: ' + (10 + 50) * 2 + ' kv. cm.');
-console.log('Perimetras: ' + (10 + 55) * 2 + ' kv. cm.');
-console.log('Perimetras: ' + (10 + 60) * 2 + ' kv. cm.');
-console.log('Perimetras: ' + (10 + 120) * 2 + ' kv. cm.');
+console.log('Perimetras: ' + (10 + 10) * 2 + ' cm.');
+console.log('Perimetras: ' + (10 + 25) * 2 + ' cm.');
+console.log('Perimetras: ' + (10 + 30) * 2 + ' cm.');
+console.log('Perimetras: ' + (10 + 35) * 2 + ' cm.');
+console.log('Perimetras: ' + (10 + 40) * 2 + ' cm.');
+console.log('Perimetras: ' + (10 + 45) * 2 + ' cm.');
+console.log('Perimetras: ' + (10 + 50) * 2 + ' cm.');
+console.log('Perimetras: ' + (10 + 55) * 2 + ' cm.');
+console.log('Perimetras: ' + (10 + 60) * 2 + ' cm.');
+console.log('Perimetras: ' + (10 + 120) * 2 + ' cm.');
 
 // DRY - Don't Repeat Yourself
 
@@ -58,3 +58,22 @@ helloWithName('John', 'Doe');
 helloWithName('Steve');
 helloWithName('Peter');
 helloWithName('Mike');
+
+
+function perimeter(width = 0, length = 0, units = 'cm') {
+  if (width && length) {
+    let answer = (width + length) * 2;
+    let output = `Perimetras: ${answer} ${units}.`;
+    console.log(output);
+  } else {
+    console.log('Įvesti neteisingi duomenys');
+  }
+}
+
+perimeter(10, 20);
+perimeter(10, 20, 'm');
+perimeter(100, 20, 'dm');
+perimeter(10, 55, 'km');
+perimeter(10, 46, 'cm');
+perimeter(10);
+perimeter();
