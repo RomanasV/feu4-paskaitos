68e50f7bd27dbbc082a3e8876dea9f7a257d2225
RomanasV <romanasvenckus@gmail.com>
2023-01-17 16:02:01 +0200

    loop tasks

--- 2023-01-17-arrays/script.js
+++ 2023-01-17-arrays/script.js
@@ -51,10 +51,57 @@ for (kintamasis; sąlyga; kintamojo pakeitimas) {
 // }
 
 
-function task1() {
-  for (let i = 1; i <= 100; i++) {
-    console.log(i);
+// Sukurti funkcijas, kurios paleidžia ciklą su skaičiais nuo 1 iki 100. Šie ciklai:
+// 1. Padaugina skaičių iš 2.
+// 2. Padaugina skaičių iš 5.
+// 3. Prideda prie skaičiaus 5.
+// 4. Atima iš skaičiaus 2.
+// 5. Pakelia skaičių kvadratu.
+// 6. Pakelia skaičių kūbu.
+
+// 7. Sukurti analogiškas funkcijas pirmoms užduotims, tačiau padaryti, jog ciklai skaičiuotųsi nuo 100 iki 1.
+
+// 8. Kiekvienos užduoties išvesties tekstą suformuluoti, jog būtų parašytas užduoties sprendimas, pvz.: 
+// 1 * 2 = 2
+// 2 * 2 = 4
+// 3 * 2 = 6
+// ir t.t.
+
+// 9. Papildyti funkcijas, jog jos priimtu šiuos argumentus:
+// 9.1. Nusakytų nuo kokio skaičiaus prasidės ciklas.
+// 9.2. Nusakytų iki kokio skaičiaus ciklas tęsis.
+// 9.3. Nusakytų kelinto ciklo metu turi išvesti atsakymą į konsolę.
+
+function task1(start = 1, end = 100, nth = 1) {
+  for (let i = start; i <= end; i += nth) {
+    // console.log(i + '*' + 2 + ' = ' + (i * 2));
+    
+    let answer = i * 2;
+    let output = `${i} * 2 = ${answer}`;
+    
+    console.log(output);
   }
 }
 
-// task1();
+// task1(1, 100, 15);
+
+function task11(start = 100, end = 1, nth = 1) {
+  for (let i = start; i >= end; i -= nth) {
+    let answer = i * 2;
+    let output = `${i} * 2 = ${answer}`;
+    
+    console.log(output);
+  }
+}
+
+// task11(100, 1, 15);
+
+function task61(start = 1, end = 100, nth = 1) {
+  for (let i = start; i <= end; i += nth) {
+    // console.log(i * i * i);
+    // console.log(Math.pow(i, 3));
+    console.log(i**3);
+  }
+}
+
+task61();
