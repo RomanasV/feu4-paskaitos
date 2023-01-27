f66d965324ba3824eb9907309e465c694d400eeb
RomanasV <romanasvenckus@gmail.com>
2023-01-27 10:06:20 +0200

    cities task end

--- 2023-01-24-objects/cities.js
+++ 2023-01-24-objects/cities.js
@@ -45,7 +45,7 @@ let citiesData = [
       continent: 'Asia',
       country: 'United Arab Emirates',
     },
-    touristAttractions: ['Burj Khalifa', 'Burj Al Arab', 'Dubai Mall'],
+    touristAttractions: ['Burj Khalifa', 'Burj Al Arab', 'Dubai Mall', 'test'],
     isCapital: false
   },
   {
@@ -55,7 +55,7 @@ let citiesData = [
       continent: 'Asia',
       country: 'Japan',
     },
-    touristAttractions: ['Senso-ji', 'Tokyo Skytree', 'Meiji Jingu'],
+    touristAttractions: ['Senso-ji', 'Tokyo Skytree', 'Meiji Jingu', 'test', 'test'],
     isCapital: true
   },
   {
@@ -109,7 +109,7 @@ function renderCities(cities) {
 
   let citiesWrapper = document.querySelector('#cities-wrapper');
 
-  cities.forEach((city) => {
+  cities.forEach((city, index) => {
     // 1.6. Visus miestų masyvų narius išvesti į konsolę.
     // console.log(city);
     // // 1.6.1. Visų miestų pavadinimis išvesti į konsolę.
@@ -176,15 +176,101 @@ renderCities(citiesData);
 
 // 5. Naudojant tik JavaScript:
 //  5.1. Pakeisti visų sostinių pavadinimo teksto spalvą.
-//  5.2. Pakeisti kas antro miesto fono spalvą.
-//  5.3. Pakeisti visų lankytinų vietų sąrašo pirmo nario spalvą į žalią.
-//  5.4. Pakeisti visų lankytinų vietų sąrašo paskutinių narių spalvą į raudoną, jeigu narių (lankytinų vietų) yra daugiau nei 3.
 
 function task51() {
-  let cityItems = document.querySelectorAll('.city-item');
+  let cityItems = document.querySelectorAll('.city-item.capital .city-title');
   cityItems.forEach((cityItem) => {
     cityItem.style.color = 'green';
   })
 }
 
 task51();
+
+//  5.2. Pakeisti kas antro miesto fono spalvą.
+
+function task52() {
+  // let cities = document.querySelectorAll('.city-item');
+
+  // cities.forEach((city, index) => {
+  //   if (index % 2 === 0) {
+  //     city.style.backgroundColor = '#bbb';
+  //   }
+  // });
+
+  // for (let i = 0; i < cities.length; i++) {
+  //   if (i % 2 === 0) {
+  //     cities[i].style.backgroundColor = '#bbb';
+  //   }
+  // }
+
+  // for (let i = 0; i < cities.length; i+=2) {
+  //   cities[i].style.backgroundColor = '#bbb';
+  // }
+
+  let cities = document.querySelectorAll('.city-item:nth-of-type(odd)');
+  cities.forEach(city => city.style.backgroundColor = '#bbb');
+}
+
+// task52();
+
+//  5.3. Pakeisti visų lankytinų vietų sąrašo pirmo nario spalvą į žalią.
+//  5.4. Pakeisti visų lankytinų vietų sąrašo paskutinių narių spalvą į raudoną, jeigu narių (lankytinų vietų) yra daugiau nei 3.
+
+function task53() { 
+  // let firstLiElements = document.querySelectorAll('.city-item li:first-of-type');
+  // let firstLiElements = document.querySelectorAll('.city-item li:first-child');
+  // firstLiElements.forEach(element => element.style.color = 'green');
+
+  // let lastLiElements = document.querySelectorAll('.city-item li:nth-of-type(1n+4)');
+  // lastLiElements.forEach(element => element.style.color = 'red');
+
+  let attractionLists = document.querySelectorAll('.city-item ul');
+  attractionLists.forEach(list => {
+    let liElements = list.querySelectorAll('li');
+    
+    liElements.forEach((li, index) => {
+      if (index === 0) {
+        li.style.color = 'green';
+      } else if (index > 2) {
+        li.style.color = 'red';
+      } else {
+        li.style.color = 'blue';
+      }
+    });
+  })
+}
+
+task53();
+
+// 6. Jeigu miestų skaičius nėra porinis, tai paskutinio miesto plotis turi būti 100%, o visų kitų - 50%.
+function task6() {
+  let citiesWrapper = document.querySelector('#cities-wrapper');
+  // citiesWrapper.style.display = 'flex';
+  // citiesWrapper.style.flexWrap = 'wrap';
+  // citiesWrapper.style.gap = '20px';
+  
+  citiesWrapper.style = `display: flex;
+                         flex-wrap: wrap;
+                         gap: 20px;`;
+  
+  let cityItems = document.querySelectorAll('.city-item');
+  console.log(cityItems);
+
+  cityItems.forEach((city, index) => {
+    // city.style = `width: calc((100% - 20px) / 2);
+    //               padding: 15px;
+    //               box-sizing: border-box;`;
+
+    city.style.width = 'calc((100% - 20px) / 2)';
+    city.style.padding = '15px';
+    city.style.boxSizing = 'border-box';
+
+    if (index + 1 === cityItems.length && cityItems.length % 2 !== 0) {
+      city.style.width = '100%';
+    }
+  })
+
+}
+
+task6();
+task52();
