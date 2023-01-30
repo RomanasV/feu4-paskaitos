7d7e3c1062cef99a17c01583f681802bb372c007
RomanasV <romanasvenckus@gmail.com>
2023-01-30 15:31:07 +0200

    +2/-2 buttons

--- 2023-01-30-event-listeners/counter.js
+++ 2023-01-30-event-listeners/counter.js
@@ -3,23 +3,35 @@ let numbersWrapper = document.getElementById('numbers');
 let numberDisplay = document.createElement('h3');
 
 let minusButton = document.createElement('button');
+let minus2Button = document.createElement('button');
 let plusButton = document.createElement('button');
+let plus2Button = document.createElement('button');
 let resetButton = document.createElement('button');
 
-numbersWrapper.append(numberDisplay, minusButton, plusButton, resetButton);
+numbersWrapper.append(numberDisplay, minus2Button, minusButton, resetButton, plusButton, plus2Button);
 
 numberDisplay.textContent = 5;
 minusButton.textContent = '-';
+minus2Button.textContent = '-2';
 plusButton.textContent = '+';
+plus2Button.textContent = '+2';
 resetButton.textContent = 'Reset';
 
 numberDisplay.style.color = 'green';
 
 minusButton.addEventListener('click', () => {
-  numberDisplay.textContent--;
+  numberDisplay.textContent = Number(numberDisplay.textContent) + 1;
   
+  if (numberDisplay.textContent < 3) {
+    minus2Button.setAttribute('disabled', true);
+  }
+
   if (numberDisplay.textContent < 2) {
     minusButton.setAttribute('disabled', true);
+  }
+
+  if (numberDisplay.textContent < 9) {
+    plus2Button.removeAttribute('disabled');
   }
   
   plusButton.removeAttribute('disabled');
@@ -31,11 +43,68 @@ minusButton.addEventListener('click', () => {
   }
 })
 
+minus2Button.addEventListener('click', () => {
+  numberDisplay.textContent = Number(numberDisplay.textContent) - 1;
+
+  if (numberDisplay.textContent < 3) {
+    minus2Button.setAttribute('disabled', true);
+  }
+
+  if (numberDisplay.textContent < 2) {
+    minusButton.setAttribute('disabled', true);
+  }
+
+  if (numberDisplay.textContent < 9) {
+    plus2Button.removeAttribute('disabled');
+  }
+
+  plusButton.removeAttribute('disabled');
+
+  if (numberDisplay.textContent > 4) {
+    numberDisplay.style.color = 'green';
+  } else {
+    numberDisplay.style.color = 'red';
+  }
+});
+
 plusButton.addEventListener('click', () => {
-  numberDisplay.textContent++;
+  numberDisplay.textContent = Number(numberDisplay.textContent) + 1;
 
   if (numberDisplay.textContent > 9) {
     plusButton.setAttribute('disabled', true);
+  }
+
+  if (numberDisplay.textContent > 8) {
+    plus2Button.setAttribute('disabled', true);
+  }
+
+  if (numberDisplay.textContent > 2) {
+    minus2Button.removeAttribute('disabled');
+  }
+
+  minusButton.removeAttribute('disabled');
+
+  if (numberDisplay.textContent > 4) {
+    numberDisplay.style.color = 'green';
+  } else {
+    numberDisplay.style.color = 'red';
+  }
+})
+
+
+plus2Button.addEventListener('click', () => {
+  numberDisplay.textContent = Number(numberDisplay.textContent) + 2;
+
+  if (numberDisplay.textContent > 9) {
+    plusButton.setAttribute('disabled', true);
+  }
+
+  if (numberDisplay.textContent > 8) {
+    plus2Button.setAttribute('disabled', true);
+  }
+
+  if (numberDisplay.textContent > 2) {
+    minus2Button.removeAttribute('disabled');
   }
 
   minusButton.removeAttribute('disabled');
@@ -50,5 +119,12 @@ plusButton.addEventListener('click', () => {
 resetButton.addEventListener('click', () => {
   numberDisplay.textContent = 5;
   plusButton.removeAttribute('disabled');
+  plus2Button.removeAttribute('disabled');
   minusButton.removeAttribute('disabled');
+  minus2Button.removeAttribute('disabled');
 })
+
+
+// 12. Sukurti du naujus mygtukus, kurie:
+// 12.1. Prideda dvejetą prie esamos h3 elemento reikšmės.
+// 12.2. Atima dvejetą iš esamos h3 elemento reikšmės.
