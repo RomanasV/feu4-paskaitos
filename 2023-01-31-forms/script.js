e6fd2f021849af1b02ca1fa57332bb2578e595fb
RomanasV <romanasvenckus@gmail.com>
2023-02-03 14:36:13 +0200

    initial students data render function

--- 2023-01-31-forms/script.js
+++ 2023-01-31-forms/script.js
@@ -26,6 +26,59 @@
 //   console.dir(event.target.elements);
 // });
 
+const initialData = [
+  {
+    name: 'Vardas 1',
+    surname: 'Pavardė 1',
+    age: 25,
+    phone: '+3704564879',
+    email: 'vardas1@gmail.com',
+    itKnowledge: 7,
+    group: 'feu 3',
+    interests: ['JavaScript', 'PHP'],
+  },
+  {
+    name: 'Vardas 2',
+    surname: 'Pavardė 2',
+    age: 32,
+    phone: '+37045645455',
+    email: 'vardas3@gmail.com',
+    itKnowledge: 10,
+    group: 'feu 1',
+    interests: ['JavaScript'],
+  },
+  {
+    name: 'Vardas 3',
+    surname: 'Pavardė 3',
+    age: 20,
+    phone: '+3704564879',
+    email: 'vardas3@gmail.com',
+    itKnowledge: 2,
+    group: 'feu 4',
+    interests: ['PHP'],
+  },
+  {
+    name: 'Vardas 5',
+    surname: 'Pavardė 5',
+    age: 40,
+    phone: '+3704564879',
+    email: 'vardas5@gmail.com',
+    itKnowledge: 4,
+    group: 'feu 3',
+    interests: [],
+  },
+  {
+    name: 'Vardas 5',
+    surname: 'Pavardė 5',
+    age: 25,
+    phone: '+3704564879',
+    email: 'vardas5@gmail.com',
+    itKnowledge: 7,
+    group: 'feu 3',
+    interests: ['JavaScript', 'PHP'],
+  },
+];
+
 const studentForm = document.querySelector('#student-form');
 // const studentList = document.getElementById('students-list');
 const studentList = document.querySelector('#students-list');
@@ -251,3 +304,102 @@ function alertMessage(element, message, color = 'black') {
     messageElement.remove();
   }, 5000)
 }
+
+function renderInitialData(students, studentForm) {
+  students.forEach(studentData => {
+    renderSingleStudent(studentData, studentForm);
+  });
+}
+
+function renderSingleStudent(student, form) {
+  const studentName = student.name;
+  const studentSurname = student.surname;
+  const studentAge = student.age;
+  const studentPhone = student.phone;
+  const studentEmail = student.email;
+  const studentItKnowledge = student.itKnowledge;
+  const studentGroup = student.group;
+  const studentInterests = student.interests;
+
+  const studentItem = document.createElement('div');
+  studentItem.classList.add('student-item');
+
+  const nameElement = document.createElement('p');
+  nameElement.innerHTML = `<strong>Name:</strong> ${studentName}`;
+
+  const surnameElement = document.createElement('p');
+  surnameElement.innerHTML = `<strong>Surname:</strong> ${studentSurname}`;
+
+  const ageElement = document.createElement('p');
+  ageElement.innerHTML = `<strong>Age:</strong> ${studentAge}`;
+
+  const phoneElement = document.createElement('p');
+  phoneElement.innerHTML = `<strong>Phone:</strong> ****`;
+
+  const emailElement = document.createElement('p');
+  emailElement.innerHTML = `<strong>Email:</strong> ****`;
+
+  const itKnowledgeElement = document.createElement('p');
+  itKnowledgeElement.innerHTML = `<strong>IT Knowledge:</strong> ${studentItKnowledge}`;
+
+  const groupElement = document.createElement('p');
+  groupElement.innerHTML = `<strong>Group:</strong> ${studentGroup.toUpperCase()} group`;
+
+  const interestsWrapper = document.createElement('div');
+  interestsWrapper.classList.add('interests-wrapper');
+
+  const interestsTitle = document.createElement('h4');
+  interestsTitle.classList.add('interests-title');
+  interestsTitle.textContent = 'No interests :(';
+  
+  interestsWrapper.append(interestsTitle);
+  
+  if (studentInterests.length > 0) {
+    interestsTitle.textContent = 'Student interests:';
+    const interestListElement = document.createElement('ul');
+
+    studentInterests.forEach(interest => {
+      const interestElement = document.createElement('li');
+      interestElement.textContent = interest;
+  
+      interestListElement.append(interestElement);
+    })
+    
+    interestsWrapper.append(interestListElement);
+  }
+
+  const privateInfoButton = document.createElement('button');
+  privateInfoButton.textContent = 'Show Private Info';
+
+  let privateInfoHidden = true;
+
+  privateInfoButton.addEventListener('click', () => {
+    
+    if (privateInfoHidden) {
+      privateInfoButton.textContent = 'Hide Private Info';
+      phoneElement.innerHTML = `<strong>Phone:</strong> ${studentPhone}`;
+      emailElement.innerHTML = `<strong>Email:</strong> ${studentEmail}`;
+    } else {
+      privateInfoButton.textContent = 'Show Private Info';
+      phoneElement.innerHTML = `<strong>Phone:</strong> ****`;
+      emailElement.innerHTML = `<strong>Email:</strong> ****`;
+    }
+
+    privateInfoHidden = !privateInfoHidden;
+  })
+
+  const removeStudentButton = document.createElement('button');
+  removeStudentButton.textContent = 'Remove Student';
+
+  removeStudentButton.addEventListener('click', () => {
+    studentItem.remove();
+
+    let removedStudentMessage = `Studentas (${studentName} ${studentSurname}) sėkmingai ištrintas.`;
+    alertMessage(form, removedStudentMessage);
+  })
+
+  studentItem.append(nameElement, surnameElement, ageElement, phoneElement, emailElement, itKnowledgeElement, groupElement, interestsWrapper, privateInfoButton, removeStudentButton);
+  studentList.prepend(studentItem);
+}
+
+renderInitialData(initialData, studentForm);
