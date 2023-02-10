6c9e74b6295e3b890aef7723f6f316cd7632c9a1
RomanasV <romanasvenckus@gmail.com>
2023-02-10 14:39:58 +0200

    async await

--- 2023-02-08-api/ai.js
+++ 2023-02-08-api/ai.js
@@ -39,34 +39,73 @@ function init() {
   const nameForm = document.querySelector('#name-form');
   const paragraph = document.createElement('p');
   
-  nameForm.addEventListener('submit', (event) => {
+  nameForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     const nameValue = nameForm.name.value;
     nameForm.after(paragraph);
 
-    paragraph.textContent = '';
     
-    fetch('https://api.agify.io/?name=' + nameValue)
-      .then(res => res.json())
-      .then(data => {
-        const age = data.age;
-        paragraph.textContent += `Person's age is ${age}. `;
 
-        fetch('https://api.genderize.io/?name=' + nameValue)
-          .then(res => res.json())
-          .then(data => {
-            const gender = data.gender;
-            paragraph.textContent += `Gender is ${gender}. `;
-    
-            fetch('https://api.nationalize.io/?name=' + nameValue)
-              .then(res => res.json())
-              .then(data => {
-                const country = data.country[0].country_id;
-                paragraph.textContent += `${nameValue}'s country is ${country}. `;
-              })
-          })
-      })
+    const ageRes = await fetch('https://api.agify.io/?name=' + nameValue);
+    const ageData = await ageRes.json();
+
+    const genderRes = await fetch('https://api.genderize.io/?name=' + nameValue);
+    const genderData = await genderRes.json();
+
+    const countryRes = await fetch('https://api.nationalize.io/?name=' + nameValue);
+    const countryData = await countryRes.json();
+
+    const age = ageData.age;
+    const gender = genderData.gender;
+    const country = countryData.country[0].country_id; 
+
+    paragraph.textContent = `Person's age is ${age}. Gender is ${gender}. ${nameValue}'s country is ${country}.`;
+
+    // fetch('https://api.agify.io/?name=' + nameValue)
+    //   .then((res) => {
+    //     return res.json();
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+      
+
+    // fetch('https://api.agify.io/?name=' + nameValue)
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     age = data.age;
+    //     paragraph.textContent += `Person's age is ${age}. `;
+    //   })
+
+    // fetch('https://api.genderize.io/?name=' + nameValue)
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     const gender = data.gender;
+    //     paragraph.textContent += `Gender is ${gender}. `;
+    //   })
+
+    // fetch('https://api.nationalize.io/?name=' + nameValue)
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     const country = data.country[0].country_id;
+    //     paragraph.textContent += `${nameValue}'s country is ${country}. `;
+    //   })
   })
 }
+
+function getAgeData(nameValue, paragraph) {
+  let age;
+
+  fetch('https://api.agify.io/?name=' + nameValue)
+    .then(res => res.json())
+    .then(data => {
+      age = data.age;
+      paragraph.textContent += `Person's age is ${age}. `;
+
+    })
+
+  return `Person's age is ${age}. `;
+}
+
 
 init();
